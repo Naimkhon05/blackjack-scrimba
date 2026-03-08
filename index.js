8331ec85e0c28306566fe0b7f7b2af08238fb6f2
Naimkhon05 <ishonbiboevnaimkhon@gmail.com>
2026-03-08 12:40:22 +0100

    adding the hasBlackJack

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ let firstCard = 9;
 let secondCard = 10;
 let sum = firstCard + secondCard;
 console.log(sum);
+let hasBlackJack = false;
 
 if (sum <= 20) {
   console.log("Do you want to draw a new card?");
 } else if (sum === 21) {
   console.log("You've got a blackjack!");
+  hasBlackJack = true;
 } else {
   console.log("You're out of the game!");
 }
