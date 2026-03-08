a24e12f622d9971adb338094f760bc1500b173b3
Naimkhon05 <ishonbiboevnaimkhon@gmail.com>
2026-03-08 12:05:53 +0100

    adding the functions

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (sum < 21) {
 
 let age = 18;
 if (age < 21) {
-  console.log("You cannot enter the club!");
+  document.getElementById("age").innerText = "You cannot enter the club!";
 } else {
-  console.log("Welcome!");
+  document.getElementById("age").innerText = "Welcome!";
 }
