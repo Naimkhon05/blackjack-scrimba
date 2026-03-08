01f3f01bb89a2030d3b066e2d0b846632d7d74db
Naimkhon05 <ishonbiboevnaimkhon@gmail.com>
2026-03-08 13:19:06 +0100

    deleting the paragraph changing the console with message variable deleting the unnecessary part of the code from js

--- index.js
+++ index.js
@@ -4,22 +4,15 @@ let sum = firstCard + secondCard;
 console.log(sum);
 let hasBlackJack = false;
 let isAlive = true;
+let message = "";
 
 if (sum <= 20) {
-  console.log("Do you want to draw a new card?");
+  message = "Do you want to draw a new card?";
 } else if (sum === 21) {
-  console.log("You've got a blackjack!");
+  message = "You've got a blackjack!";
   hasBlackJack = true;
 } else {
-  console.log("You're out of the game!");
+  message = "You're out of the game!";
   isAlive = false;
 }
-console.log(hasBlackJack);
-console.log(isAlive);
-
-let age = 18;
-if (age < 21) {
-  document.getElementById("age").textContent = "You cannot enter the club!";
-} else {
-  document.getElementById("age").textContent = "Welcome!";
-}
+console.log(message);
