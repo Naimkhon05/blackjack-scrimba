fea7f59dbf903486b89a7b3f0b990aee671f3cb8
Naimkhon05 <ishonbiboevnaimkhon@gmail.com>
2026-03-21 18:04:40 +0100

    adding the start game button , decorating and connecting to JS

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "You've got a blackjack!";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game!";
-  isAlive = false;
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got a blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+
+  console.log(message);
 }
-console.log(message);
